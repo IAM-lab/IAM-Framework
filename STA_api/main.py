dbb6b06c728710f709e6002d325858dca05c61de
Aayush Chadha <achadharocks@gmail.com>
2019-02-08 12:30:31 +0000

    Add STA call from API

--- STA_api/main.py
+++ STA_api/main.py
@@ -1,16 +1,81 @@
 from flask import Flask, request, Response, jsonify
 from os import environ
+from Utils import utils, stringprocessing
+from AOI import aoi
+from Sequences import sequences
+from Fixation import fixation
 
 app = Flask(__name__)
 
 
-@app.route('/<eyeTrackingURL>/<degreeOfAccuracy>/<distanceBetweenEyeTrackerAndParticipants>/'
+
+@app.route('/<pList>/<eyeTrackingURL>/<degreeOfAccuracy>/<distanceBetweenEyeTrackerAndParticipants>/'
            + '<resolutionOfScreenX>/<resolutionOfScreenY>/<sizeOfScreen>', methods=['POST'])
-def sta_request(eyeTrackingURL, degreeOfAccuracy, distanceBetweenEyeTrackerAndParticipants,
-               resolutionOfScreenX, resolutionOfScreenY, sizeOfScreen):
-    print (request.get_data())
-    print(eyeTrackingURL, degreeOfAccuracy, distanceBetweenEyeTrackerAndParticipants)
+def sta_request(pList, eyeTrackingURL, degreeOfAccuracy, distanceBetweenEyeTrackerAndParticipants,
+                resolutionOfScreenX, resolutionOfScreenY, sizeOfScreen):
+    # We receive the input body as a huge b64 encoded string separated by a \n signifying
+    # the start of the segmentation file, we can split the here to pass it to our string processing
+    #  function which cleans it up and allows the other modules to use it
+    datafiles = request.get_data().decode("utf-8").split("\n")
+
+    segmentation_file = stringprocessing.getSegmentationFile(datafiles[1])
+
+    participant_files = stringprocessing.getParticipantFiles(datafiles[0])
+
+
+
+    print(pList, eyeTrackingURL, degreeOfAccuracy, distanceBetweenEyeTrackerAndParticipants, resolutionOfScreenX,
+          resolutionOfScreenY, sizeOfScreen)
     return Response()
+
+
+def sta(pList, EyeTrackingFiles, EyeTrackingURL, SegmentationPath,
+        degreeOfAccuracy, distanceBetweenEyeTrackerAndParticipants,
+        resolutionOfScreenX, resolutionOfScreenY, sizeOfScreen):
+    # STA Algorithm
+
+    # Preliminary Stage
+    myParticipants = utils.getParticipants(pList, EyeTrackingFiles, EyeTrackingURL)
+    myAoIs = aoi.getAoIs(SegmentationPath)
+    myErrorRateArea = utils.calculateErrorRateArea(degreeOfAccuracy, distanceBetweenEyeTrackerAndParticipants,
+                                             resolutionOfScreenX, resolutionOfScreenY, sizeOfScreen)
+    mySequences = sequences.createSequences(myParticipants, myAoIs, myErrorRateArea)
+
+    keys = mySequences.keys()
+    for y in range(0, len(keys)):
+        mySequences[keys[y]] = mySequences[keys[y]].split('.')
+        del mySequences[keys[y]][len(mySequences[keys[y]]) - 1]
+    for y in range(0, len(keys)):
+        for z in range(0, len(mySequences[keys[y]])):
+            mySequences[keys[y]][z] = mySequences[keys[y]][z].split('-')
+
+    # First-Pass
+    mySequences_num = {}
+    keys = mySequences.keys()
+    for y in range(0, len(keys)):
+        if (len(mySequences[keys[y]]) != 0):
+            mySequences_num[keys[y]] = sequences.getNumberedSequence(mySequences[keys[y]])
+        else:
+            mySequences_num[keys[y]] = []
+
+    myImportanceThreshold = utils.calculateImportanceThreshold(mySequences_num)
+    myImportantAoIs = aoi.updateAoIsFlag(aoi.getNumberDurationOfAoIs(mySequences_num), myImportanceThreshold)
+    myNewSequences = aoi.removeInsignificantAoIs(mySequences_num, myImportantAoIs)
+
+    # Second-Pass
+    myNewAoIList = aoi.getExistingAoIList(myNewSequences)
+    myNewAoIList = fixation.calculateTotalNumberDurationofFixationsandNSV(
+        myNewAoIList, fixation.calculateNumberDurationOfFixationsAndNSV(
+        myNewSequences))
+    myFinalList = aoi.getValueableAoIs(myNewAoIList)
+    myFinalList.sort(key=lambda x: (x[4], x[3], x[2]))
+    myFinalList.reverse()
+
+    commonSequence = []
+    for y in range(0, len(myFinalList)):
+        commonSequence.append(myFinalList[y][0])
+
+    return sequences.getAbstractedSequence(commonSequence)
 
 
 if __name__ == '__main__':
