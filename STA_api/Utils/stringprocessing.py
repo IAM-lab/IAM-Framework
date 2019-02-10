06165730fa09fc586ebeb54c69ebe101c65d6ec3
Aayush Chadha <achadharocks@gmail.com>
2019-02-10 23:05:05 +0000

    Handle lists of dictionaries

--- STA_api/Utils/stringprocessing.py
+++ STA_api/Utils/stringprocessing.py
@@ -1,24 +1,41 @@
 import base64
 
-def getSegmentationFile(encodedSegmentationFile):
+def base64Decode(base64String):
     """
 
     :param encodedSegmentationFile:
     :return decodedFileString
     """
 
-    return base64.b64decode(encodedSegmentationFile)
+    return base64.b64decode(base64String)
 
-
-def getParticipantFiles(encodedParticipantFiles):
+# TODO function expects that there is a one to one match between pList and encodedParticipantFiles
+# TODO i.e, participant id -> file
+def getParticipantFiles(pList, encodedParticipantFiles):
 
     """
 
     :param encodedParticipantFiles:
-    :return participantFileList: List containing decoded participant files
+    :return participantFileList: List containing dictionaries of decoded participant files
     """
 
     decodedFiles = base64.b64decode(encodedParticipantFiles)
     participantFileList = decodedFiles.decode().split("== NEW PARTICIPANT FILE ==")
+    participantData = {}
+    participantDataList = []
 
-    return participantFileList
+    # We are going to access participant data by key to their file, however, we first need to link the
+    #  files by participant id
+    participantFileIndex = 0
+
+    # TODO Add error handling for lengths mismatch
+
+    print(participantFileList)
+
+    for participantId in pList:
+        participantData["id"] = participantId
+        participantData["eye_tracking_data"] = participantFileList[participantFileIndex]
+        participantFileIndex += 1
+        participantDataList.append(participantData)
+
+    return participantDataList
